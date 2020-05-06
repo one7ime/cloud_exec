458cb160dfac648b9a3025e669e1264b2c1b6a6e
psh <one7ime@naver.com>
2020-05-06 18:49:14 +0900

    4rd commit

--- k8s/k8s.sh
+++ k8s/k8s.sh
@@ -1,10 +1,6 @@
-vagrant init
-vagrant up
-vagrant ssh node1
 kubectl run nginx --image=one7ime/cloud_exec:0.8 --port=80
 kubectl expose deploy/nginx --type="NodePort" --port 80
 kubectl scale deploy nginx --replicas=20
 kubectl get svc
-sleep 2
 kubectl get pods -o wide
 
